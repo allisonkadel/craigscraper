1c701c2d6f42eadcf4a39fb7be7a3deaaebc8f5a
allisonkadel <akadel01@gmail.com>
2019-10-22 16:43:11 -0600

    Create gem scaffold

---
+++ lib/craigscraper.rb
@@ -0,0 +1,6 @@
+require "craigscraper/version"
+
+module Craigscraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/craigscraper/version.rb
@@ -0,0 +1,3 @@
+module Craigscraper
+  VERSION = "0.1.0"
+end
